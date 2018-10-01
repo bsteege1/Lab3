0de7f1c2244030f27af239dbd4df6a7e526bf360
bsteege1 <43155704+bsteege1@users.noreply.github.com>
2018-10-01 17:50:14 -0600

    Durring Lab

--- Lab3-bsteege1/Lab3-bsteege1/Lab3-bsteege1.cpp
+++ Lab3-bsteege1/Lab3-bsteege1/Lab3-bsteege1.cpp
@@ -10,7 +10,8 @@ using std::endl;
 
 int main()
 {
-/*	short n;
+	/*
+	short n;
 	short sum(0);
 	
 	cout << "Please enter a value for n <= 255: ";
@@ -22,7 +23,7 @@ int main()
 		sum = sum + x;
 		cout << "Sum: " << sum << endl << "n: " << n << endl;
 	}
-
+	
 	long n1;
 	long sum1(0);
 
@@ -38,18 +39,18 @@ int main()
 	
 
 	long n2;
-	long product(1);
+	long product2(1);
 
-	cout << "Please enter a value for n <= 65535: ";
+	cout << "Please enter a value for n <= 17: ";
 	cin >> n2;
 
 	for (long i(1); i <= n2; i++)
 	{
-		if (product > 0)
+		if (product2 > 0)
 		{
 			long x1 = i;
-			product = product * x1;
-			cout << "Product: " << product << endl << "i: " << i << endl;
+			product2 = product2 * x1;
+			cout << "Product: " << product2 << endl << "i: " << i << endl;
 		}
 		else
 		{
@@ -59,26 +60,66 @@ int main()
 		}
 	}
 
-	*/
-
+	
+	
 	float n3;
-	float product(1);
+	float product3(1);
 
 	cout << "Please enter a value for n <= 65535: ";
 	cin >> n3;
 
 	for (float i(1); i <= n3; i++)
 	{
-		if (product > 0)
+		if (product3 > 0)
 		{
 			float x1 = i;
-			product = product * x1;
-			cout << "Product: " << product << endl << "i: " << i << endl;
+			product3 = product3 * x1;
+			cout << "Product: " << product3 << endl << "i: " << i << endl;
 		}
 		else
 		{
 			cout << "Overflow begins at: " << n3 << endl;
 		}
+	}
+	
+
+	float n4;
+	float sum4(0);
+
+	cout << "Please enter a value for n <= 8: ";
+	cin >> n4;
+
+	for (float i(1); i <= n4; i++)
+	{
+		float ratio = (1 / n4);
+		sum4 = (ratio + sum4);
+		cout << "Sum: " << (sum4 - 1) << endl << "i: " << i << endl;
+	}
+	
+	double n5;
+	double sum5(0);
+
+	cout << "Please enter a value for n: ";
+	cin >> n5;
+
+	for (double i(1); i <= n5; i++)
+	{
+		double ratio = (1 / n5);
+		sum5 = (ratio + sum5);
+		cout << "Sum: " << (sum5 - 1) << endl << "i: " << i << endl;
+	}
+	*/
+	
+	//The float does not store the exact value, which makes the for loop believe that i is actually less than 4.4, when if it was more accurate, it shouldn't have. The double is far more accurate 
+	// and will actually go to the correct values. 
+	for (float i = 3.4; i < 4.4; i += 0.2) 
+	{ 
+		cout << "i = " << i << endl;
+	}
+
+	for (double i = 3.4; i < 4.4; i += 0.2)
+	{
+		cout << "i = " << i << endl;
 	}
 	
 	system("pause");
